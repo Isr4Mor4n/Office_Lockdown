996b0034a9287f74ca6ab9f6cdba3753e0c5e828
Kevin Cabrera <zap473@amerike.edu.mx>
2024-03-10 20:11:21 -0600

    Puzzle
    First Keypad Puzzle

---
+++ Assets/Keypad System/Scripts/Managers - One Per Scene/KPDisableManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace KeypadSystem
+{
+    public class KPDisableManager : MonoBehaviour
+    {
+        // [SerializeField] private FirstPersonController player = null;
+        [SerializeField] private PlayerController _player = null;
+
+
+        public static KPDisableManager instance;
+
+        void Awake()
+        {
+            if (instance != null) { Destroy(gameObject); }
+            else { instance = this; DontDestroyOnLoad(gameObject); }
+        }
+
+        
+        public void DisablePlayer(bool disable)
+        {
+            _player.enabled = !disable;
+           // KPUIManager.instance.ShowCrosshair(disable);
+        }
+
+        /*
+        public void EnablePlayer(bool enable)
+        {
+            _player.enabled = enable;
+        }
+        */
+    }
+}
---
+++ Assets/Keypad System/Scripts/Trigger Interaction (NOT REQUIRED)/KeypadTrigger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace KeypadSystem
+{
+    public class KeypadTrigger : MonoBehaviour
+    {
+        [Header("Keypad Object")]
+        [SerializeField] private KeypadItem keypadObject = null;
+
+        private const string playerTag = "Player";
+        private bool canUse;
+
+        private void Update()
+        {
+            ShowKeypadUI();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(playerTag))
+            {
+                Debug.Log("Player in zone");
+                canUse = true;
+                KPUIManager.instance.ShowInteractPrompt(canUse);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(playerTag))
+            {
+                    canUse = false;
+                    KPUIManager.instance.ShowInteractPrompt(canUse);
+            }
+        }
+
+        void ShowKeypadUI()
+        {
+            if (canUse)
+            {
+
+                keypadObject.ShowKeypadUI();
+                canUse = false; // Restablecer canUse después de mostrar la interfaz
+                KPUIManager.instance.ShowInteractPrompt(false);
+            }
+        }
+    }
+}
